4bd9267ba349db29f3c017be0bf519dc36e5f136
Chris Dosé <chris@peek.com>
2016-05-19 14:47:41 -0700

    Code review cleanup.

--- spec/serializer_spec.rb
+++ spec/serializer_spec.rb
@@ -132,12 +132,16 @@ describe JSONAPI::Serializer do
       it 'does not include id when it is nil' do
         post = create(:post)
         post.id = nil
-        primary_data = serialize_primary(post, {serializer: MyApp::SimplestPostSerializerWithoutSelfLink})
+        primary_data = serialize_primary(post, {serializer: MyApp::PostSerializerWithoutLinks})
         expect(primary_data).to eq({
           'type' => 'posts',
           'attributes' => {
             'title' => 'Title for Post 1',
             'long-content' => 'Body for Post 1',
+          },
+          'relationships' => {
+            'author' => {},
+            'long-comments' => {},
           },
         })
       end
